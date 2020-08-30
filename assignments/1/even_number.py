7ecdc21e66d25fe5be0c58645127673afa65ce9e
Danny Meijer <danny.meijer@nike.com>
2020-08-30 13:31:50 +0200

    assignment 1

---
+++ assignments/1/even_number.py
@@ -0,0 +1,24 @@
+"""
+Assignment 1:
+ - Check is the given number is even or not.
+ - Your function should return True if the number is even, False if the number is odd.
+ - Name your function `is_even`
+
+Input: An integer
+Output: A boolean
+
+Example:
+ > is_even(2) == True
+ > is_even(5) == False
+ > is_even(0) == True
+"""
+
+
+...
+
+
+# tests
+if __name__ == '__main__':
+    assert is_even(2) is True
+    assert is_even(5) is False
+    assert is_even(0) is True
