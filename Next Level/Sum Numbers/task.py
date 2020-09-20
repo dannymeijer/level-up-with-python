fcf0df2da2d1d183a683acc0f3d1e778ef71df05
Danny Meijer <danny.meijer@nike.com>
2020-09-20 13:40:28 +0200

    Next level

--- Next Level/Sum Numbers/task.py
+++ Next Level/Sum Numbers/task.py
@@ -1,3 +1,28 @@
+"""
+In a given text you need to sum the numbers. Only separated numbers should
+ be counted. If a number is part of a word it shouldn't be counted.
+
+The text consists from numbers, spaces and english letters
+
+Input: A string.
+
+Output: An int.
+
+Example:
+
+sum_numbers('hi') == 0
+sum_numbers('who is 1st here') == 0
+sum_numbers('my numbers is 2') == 2
+count_digits(
+    'This picture is an oil on canvas '
+    'painting by Danish artist Anna '
+    'Petersen between 1845 and 1910 year'
+) == 3755
+sum_numbers('5 plus 6 is') == 11
+sum_numbers('') == 0
+"""
+
+
 def sum_numbers(text: str) -> int:
     # your code goes here
     pass
